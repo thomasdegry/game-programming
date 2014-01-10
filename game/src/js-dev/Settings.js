406060818edbcd6ecc93ff497aa5737fcb19ae03
Thomas Degry <degry.thomas@gmail.com>
2014-01-10 02:24:04 +0100

    Fixed language error in tips

--- game/src/js-dev/Settings.js
+++ game/src/js-dev/Settings.js
@@ -3,7 +3,7 @@ var Settings =(function () {
     var Settings = function () {
         this.hostName = 'http://192.168.0.247';
         this.debug = true;
-        this.tips = ['Maybe you should try not to kiss planets?', 'UFO\'s aren\'t that fond of rockets..', 'You can do better! Try agian! C\'mon!', 'I agree it can be hot in the rocket.. But this?', 'Can we agree to not do this from now on?'];
+        this.tips = ['Maybe you should try not to kiss planets?', 'UFO\'s aren\'t that fond of rockets..', 'You can do better! Try agian! C\'mon!', 'I agree it can be hot inside the rocket.. But this?', 'Can we agree to not do this from now on?'];
     };
 
     return Settings;
